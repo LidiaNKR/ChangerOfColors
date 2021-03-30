8fbbf9d658639a18b55c5655ba7428abcc7d594e
LidiaNKR <77931445+LidiaNKR@users.noreply.github.com>
2021-03-30 20:08:03 +0300

    Initial Commit

---
+++ ChangerOfColors/ChangerOfColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangerOfColorsApp.swift
+//  ChangerOfColors
+//
+//  Created by Лидия Ладанюк on 30.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChangerOfColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
